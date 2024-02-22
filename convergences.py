52a7f70bf20b6c6bf96dcd2e60fd466133889dd4
Gonzalo Vega <gon2d3@gmail.com>
2024-02-22 19:23:53 +0100

    Ask for algorithm to analyze

--- convergences.py
+++ convergences.py
@@ -27,11 +27,27 @@ filepath = os.path.join(home_route, filename)
 
 data = pd.read_csv(filepath)
 
+# Preguntar al usuario por el número 1 o 2
+algorithm_choice = input(Color.BOLD + "\nSelecciona el algoritmo evolutivo a analizar: \n 1) DE - Differential Evolution (Default). \n 2) PSO - Particle Swarm Optimization. " + Color.END)
+
+# Validar la entrada del usuario
+while algorithm_choice not in ['1', '2']:
+    print(Color.RED + "\nEntrada no válida. Por favor, seleccione el número 1 o el número 2." + Color.END)
+    algorithm_choice = input(Color.BOLD + "\nSelecciona el algoritmo evolutivo a analizar: \n 1) DE - Differential Evolution (Default). \n 2) PSO - Particle Swarm Optimization.\n " + Color.END)
+
+# Convertir la opción del usuario a tipo entero
+algorithm_choice = int(algorithm_choice)
+
 # Ordenar el DataFrame por la primera columna (ID del objeto)
 data_sorted = data.sort_values(by='id_cloud')
 
+# Filtrar los datos según la opción seleccionada por el usuario
+filtered_data = data_sorted[data_sorted['algorithm'] == algorithm_choice]
+
+total_rows_analyzed = len(filtered_data)
+
 # Obtener los valores únicos de la primera columna
-unique_ids = data_sorted['id_cloud'].unique()
+unique_ids = filtered_data['id_cloud'].unique()
 
 # Crear un diccionario para almacenar el recuento de errores y el número total de muestras por grupo
 error_counts = {}
@@ -39,23 +55,23 @@ error_counts = {}
 # Iterar sobre los valores únicos de la primera columna
 for unique_id in unique_ids:
     # Filtrar el DataFrame por el valor único de la primera columna
-    filtered_data = data_sorted[data_sorted['id_cloud'] == unique_id]
+    id_filtered_data = filtered_data[filtered_data['id_cloud'] == unique_id]
     
     # Contar el número de veces que todas las columnas son menores que sus umbrales correspondientes
-    error_count = ((filtered_data['poserror'] < POSERROR_THRESHOLD) &
-                   (filtered_data['orierror_1'] < ORIERRROR_1_THRESHOLD) &
-                   (filtered_data['orierror_2'] < ORIERRROR_2_THRESHOLD) &
-                   (filtered_data['orierror_3'] < ORIERRROR_3_THRESHOLD)).sum()
+    error_count = ((id_filtered_data['poserror'] < POSERROR_THRESHOLD) &
+                   (id_filtered_data['orierror_1'] < ORIERRROR_1_THRESHOLD) &
+                   (id_filtered_data['orierror_2'] < ORIERRROR_2_THRESHOLD) &
+                   (id_filtered_data['orierror_3'] < ORIERRROR_3_THRESHOLD)).sum()
     
     # Obtener el número total de muestras en este grupo
-    total_samples = len(filtered_data)
+    total_samples = len(id_filtered_data)
     
     # Almacenar el recuento de errores y el número total de muestras en el diccionario
     error_counts[unique_id] = (error_count, total_samples)
 
 # Imprimir la proporción de veces que se cumple la condición sobre el número total de muestras para cada grupo
 print(Color.CYAN + f"\nErrores máximos para convergencia [posición, alfa, beta, theta]: {POSERROR_THRESHOLD}m, {ORIERRROR_1_THRESHOLD}º, {ORIERRROR_2_THRESHOLD}º, {ORIERRROR_3_THRESHOLD}º" + Color.END)
-print(Color.YELLOW + "\nNumero de veces que converge cada nube de puntos:" + Color.END)
+print(Color.YELLOW + f"\nNumero de veces que converge cada nube de puntos (Total: {total_rows_analyzed}):" + Color.END)
 for unique_id, (error_count, total_samples) in error_counts.items():
     if total_samples > 0:
         ratio = error_count / total_samples
