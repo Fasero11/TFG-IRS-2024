c6e0cc26ef578b045b56cb0d9e72a54f8fa45c23
Gonzalo Vega <gon2d3@gmail.com>
2024-06-28 01:23:57 +0200

    update

--- evloc/evloc/evloc_node.py
+++ evloc/evloc/evloc_node.py
@@ -25,12 +25,14 @@ import time
 
 PACKAGE_PATH = os.path.join(get_package_share_directory('evloc'), 'resources')
 
-GROUNDTRUTH_FILE_PATH = f"{PACKAGE_PATH}/groundtruth_data.csv"
-LOCAL_CLOUDS_FOLDER = f"{PACKAGE_PATH}/local_clouds"
+GROUNDTRUTH_FILE_PATH = f"{PACKAGE_PATH}/sim_groundtruth_data.csv"
+LOCAL_CLOUDS_FOLDER = f"{PACKAGE_PATH}/sim_local_clouds"
 
-DOWN_SAMPLING_FACTOR_GLOBAL = 0.004     # factor de downsampling para mapa, hay que reducir puntos en ambas nubes
-DOWN_SAMPLING_FACTOR = 0.01             # factor de downsampling para scan
+DOWN_SAMPLING_FACTOR_GLOBAL = 0.003 #0.004     # factor de downsampling para mapa, hay que reducir puntos en ambas nubes
+DOWN_SAMPLING_FACTOR = 1 #0.01             # factor de downsampling para scan
 POP_RATIO = 0.01
+
+MAX_CLOUDS = 4 # 44
 
 ########################################
 
@@ -169,11 +171,14 @@ class PCD(Node):
             global_downsample = 1
 
         else:
-            map_global_ori = o3d.io.read_point_cloud(f"{PACKAGE_PATH}/map_global_ori.ply")
-            map_global = map_global_ori.uniform_down_sample(every_k_points=int(1 / DOWN_SAMPLING_FACTOR_GLOBAL)) # Original PointCloud (Global Map)
-            global_downsample = 5
+            # map_global_ori = o3d.io.read_point_cloud(f"{PACKAGE_PATH}/map_global_ori.ply")
+            # map_global = map_global_ori.uniform_down_sample(every_k_points=int(1 / DOWN_SAMPLING_FACTOR_GLOBAL)) # Original PointCloud (Global Map)
+            # global_downsample = 5
 
-        
+            map_global_unfiltered = o3d.io.read_point_cloud(f"{PACKAGE_PATH}/map_global_sim.pcd")
+            map_global = filter_map_height(map_global_unfiltered, 0, 1.35)
+            global_downsample = 1
+
         points2 = np.asarray(map_global.points)[::global_downsample] # Downsampling. Son demasiados puntos para RVIZ
         pcd_global = self.point_cloud(points2, fixed_frame)
         self.pcd_publisher_global.publish(pcd_global)
@@ -283,7 +288,7 @@ class PCD(Node):
                 if not self.simulated:
                     # Loop for every cloud when in auto mode
                     id_cloud += 1
-                    if id_cloud > 44:
+                    if id_cloud > MAX_CLOUDS:
                         id_cloud = 1
 
             print(Color.BOLD + "\n------------------------------------" + Color.END)
